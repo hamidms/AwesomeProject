2ba0f518e9ce231f1f70b4ff49170633fd69eba0
Hamid Sukardi <hamid@Hamids-MacBook-Pro.local>
2023-12-15 21:49:47 +0700

    update script package.json and App.js

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { Text } from "react-native";
-
+import { Image, Text, TextInput, View } from "react-native";
 
 const App = () => {
-  return <Text>Hello Hamid</Text>;
+  return (
+    <View>
+      <View style={{ width: 80, height: 80, backgroundColor: "#0abde3"}}/>
+      <Text>Hamid</Text>
+      <Text>Machfudin</Text>
+      <Hamid/>
+      <Photo/>
+      <TextInput style={{borderWidth: 1}} />
+    </View>
+  )
+}
+
+const Hamid = () => {
+  return <Text>Sukardi Ok</Text>
+}
+
+const Photo = () => {
+  return <Image 
+    source={{uri: 'https://loremflickr.com/320/240'}} 
+    style={{ width: 100, height: 100}}
+  />
 }
 
 export default App;
